ea3e298ad855e01b5d1131f684747f526d2d88db
stesun2 <ss27@illinois.edu>
2021-07-20 15:53:30 -0500

    completed whack-a-mole.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'
 import './App.css'
 import Mole from './components/mole/Mole.js'
+// import { useState } from 'react'
 
 class App extends Component {
+  // const [isVisible, setVisible] = useState(false)
+
+  // 
   state = {
     dens: this.getDensState(),
     points: 0,
@@ -17,6 +21,7 @@ class App extends Component {
       })
     }, 1500)
   }
+  // Filling up the den randomly with T of F values for each?
   getDensState() {
     return new Array(9).fill({}).map(() => {
       return { 
@@ -24,7 +29,8 @@ class App extends Component {
       }
     })
   }
-  onMoleWhacked() {
+  // 
+  onMoleWhacked = () => {
     this.setState({
       points: this.state.points + 1
     })
@@ -32,7 +38,7 @@ class App extends Component {
   render() {
     const dens = this.state.dens.map((den, index) => {
       return (
-        <Mole key={`mole-${index}`} />
+        <Mole key={`mole-${index}`} den={ den } onMoleWhacked={ this.onMoleWhacked } />
       )
     })
     return (
--- src/components/mole/Mole.js
+++ src/components/mole/Mole.js
@@ -4,9 +4,13 @@ import MoleIcon from './Mole.svg'
 
 class Mole extends Component {
   render() {
+    console.log(this.props)
     return (
       <div className="den">
-        <img src={MoleIcon} className="Mole" alt="Mole" />
+        
+        {/* <img src= className="Mole" alt="Mole" /> */}
+         {this.props.den.isMoleVisible ? <img src={MoleIcon} onClick={this.props.onMoleWhacked} className="Mole" alt="Mole" /> : null }
+         {/* {this.props.den.isMoleVisible ? this.props.onMoleWhacked : null} */}
       </div>
     )
   }
